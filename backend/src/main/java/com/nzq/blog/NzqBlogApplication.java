93cd5487c4560c57a0e04ca9410519410946bce3
niezhiqiang <niezhiqiang0419@163.com>
2026-04-14 20:53:46 +0800

    add src

---
+++ backend/src/main/java/com/nzq/blog/NzqBlogApplication.java
@@ -0,0 +1,12 @@
+package com.nzq.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NzqBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NzqBlogApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/nzq/blog/NzqBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nzq.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NzqBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
